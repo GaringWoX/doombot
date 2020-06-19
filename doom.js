7dc6a115e6c37896625392e0c2a57abe43a3852a
GaringWoX <65514361+GaringWoX@users.noreply.github.com>
2020-06-19 11:46:14 +0700

    Update doom.js

--- doom.js
+++ doom.js
@@ -1,3 +1,37 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({disableEveryone: true});
 const Request = require("request");
+const client = new Discord.Client({
+    disableEveryone: true
+});
+const config = require("./config.json");
+
+client.commands = new Discord.Collection();
+
+client.on('message', (message) = > {
+
+        if (!message.content === `$ {
+            prefix
+        }`) return;
+        if (message.channel.type === "dm") return;
+        if (message.author.bot) return;
+
+        const messageArray = message.content.split(" ");
+        const command = messageArray[0];
+        const args = messageArray.slice(1);
+
+        let commands = client.commands.get(command.slice(prefix.length));
+        if (commands) commands.run(client, message, args);
+
+        if (message.content === `$ {
+                prefix
+            }
+            ping`) {
+            message.channel.send('Pong! ' + `$ {
+                message.client.ping
+            }` + 'ms');
+        }
+    };
+
+    console.log('Connected Successfully');
+
+    client.login(process.env.Token)
